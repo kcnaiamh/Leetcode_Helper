6df1ff395c133a8d05672592e85154693f5f9579
NaimulIslam9m <naimul9m@gmail.com>
2023-08-20 18:27:26 +0600

    bug fixed, code base updated

--- MainFrame.java
+++ MainFrame.java
@@ -1,33 +1,28 @@
 import java.awt.Cursor;
 import java.awt.Container;
 import java.awt.event.ActionListener;
+import java.util.HashSet;
+import java.util.Vector;
 import java.awt.event.ActionEvent;
 
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-
-import javax.swing.JOptionPane;
 
 class MainFrame extends JFrame {
 
     private Container container;
-    private JRadioButton array, string, oneDimension, twoDimension, threeDimension;
-    private ButtonGroup buttonGroup1, buttonGroup2;
     private JLabel label1, label2;
     private JScrollPane scrollPane1, scrollPane2;
-    private JButton clearButton, runButton;
+    private JButton clearButton, runButton, stringButton;
     private JTextArea textArea1, textArea2;
-
-    private int[] selectedValue = new int[2];
+    private Vector<Integer> vecSize = new Vector<Integer>();
 
     private MainFrame() {
         initContainer();
-        initRadioButton();
         initLabel();
         initTextArea();
         initButton();
@@ -38,149 +33,135 @@ class MainFrame extends JFrame {
         container.setLayout(null);
     }
 
-    private void initRadioButton() {
-
-        buttonGroup1 = new ButtonGroup();
-        buttonGroup2 = new ButtonGroup();
-
-        array = new JRadioButton("Array");
-        array.setBounds(20, 20, 70, 20);
-        array.setSelected(true);
-        container.add(array);
-
-        string = new JRadioButton("String");
-        string.setBounds(20, 50, 70, 20);
-        container.add(string);
-
-        buttonGroup1.add(array);
-        buttonGroup1.add(string);
-
-        oneDimension = new JRadioButton("1D");
-        oneDimension.setBounds(150, 20, 50, 20);
-        oneDimension.setSelected(true);
-        container.add(oneDimension);
-
-        twoDimension = new JRadioButton("2D");
-        twoDimension.setBounds(150, 50, 50, 20);
-        container.add(twoDimension);
-
-        threeDimension = new JRadioButton("3D");
-        threeDimension.setBounds(150, 80, 50, 20);
-        container.add(threeDimension);
-
-        buttonGroup2.add(oneDimension);
-        buttonGroup2.add(twoDimension);
-        buttonGroup2.add(threeDimension);
-
-        LRadioButton1 listener1 = new LRadioButton1();
-        LRadioButton2 listener2 = new LRadioButton2();
-
-        array.addActionListener(listener1);
-        string.addActionListener(listener1);
-        oneDimension.addActionListener(listener2);
-        twoDimension.addActionListener(listener2);
-        threeDimension.addActionListener(listener2);
-    }
-
     private void initLabel() {
         label1 = new JLabel("Enter Testcase");
-        label1.setBounds(20, 120, 150, 20);
+        label1.setBounds(20, 20, 150, 20);
         container.add(label1);
 
         label2 = new JLabel("Output");
-        label2.setBounds(20, 350, 100, 20);
+        label2.setBounds(20, 250, 100, 20);
         container.add(label2);
     }
 
     private void initTextArea() {
-        textArea1 = new JTextArea();
-        textArea1.setLineWrap(true);
-        textArea1.setWrapStyleWord(true);
+        createTextAreas();
+    }
 
-        scrollPane1 = new JScrollPane(textArea1, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        scrollPane1.setBounds(20, 150, 560, 180);
-        container.add(scrollPane1);
+    private void createTextAreas() {
+        textArea1 = createTextArea();
+        scrollPane1 = createScrollPane(textArea1, 20, 50, 560, 180);
 
+        textArea2 = createTextArea();
+        scrollPane2 = createScrollPane(textArea2, 20, 280, 560, 180);
+    }
 
-        textArea2 = new JTextArea();
-        textArea2.setLineWrap(true);
-        textArea2.setWrapStyleWord(true);
+    private JTextArea createTextArea() {
+        JTextArea textArea = new JTextArea();
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        return textArea;
+    }
 
-        scrollPane2 = new JScrollPane(textArea2, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        scrollPane2.setBounds(20, 380, 560, 180);
-        container.add(scrollPane2);
-
+    private JScrollPane createScrollPane(JComponent component, int x, int y, int width, int height) {
+        JScrollPane scrollPane = new JScrollPane(component, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPane.setBounds(x, y, width, height);
+        container.add(scrollPane);
+        return scrollPane;
     }
 
     private void initButton() {
-        clearButton = new JButton("CLEAR");
-        clearButton.setBounds(500, 580, 80, 20);
-        clearButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        container.add(clearButton);
+        createButtons();
+        addActionListenersToButtons();
+    }
 
-        runButton = new JButton("RUN");
-        runButton.setBounds(400, 580, 80, 20);
-        runButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        container.add(runButton);
+    private void createButtons() {
+        runButton = createButton("Run", 300, 480);
+        stringButton = createButton("String", 400, 480);
+        clearButton = createButton("Clear", 500, 480);
+    }
 
+    private JButton createButton(String label, int x, int y) {
+        JButton button = new JButton(label);
+        button.setBounds(x, y, 80, 20);
+        button.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        container.add(button);
+        return button;
+    }
+
+    private void addActionListenersToButtons() {
         runButton.addActionListener(new LRunButton());
         clearButton.addActionListener(new LClearButton());
+        stringButton.addActionListener(new LConvertStringButton());
     }
 
     String getFromatedText(String s) {
-        if (selectedValue[0] == 0) {
-            if (selectedValue[1] == 0) {
-                return s.substring(1, s.length() - 1).replaceAll("[,]+", " ");
-            } else if (selectedValue[1] == 1) {
-                s = s.substring(2, s.length() - 1);
-                String res = "";
-                int pos = s.indexOf("]"), cur = 0;
-                while (pos != -1) {
-                    res = res.concat(s.substring(cur, pos).replaceAll("[,]+", " ").concat("\n"));
-                    cur = pos + 3;
-                    pos = s.indexOf("]", cur);
-                }
-                return res;
-            } else {
-                JOptionPane.showMessageDialog(null, "its a dummy :3, Inshaallah I will work with it", "Title", JOptionPane.INFORMATION_MESSAGE);
+        int cnt = 1;
+
+        if (s.substring(0, 2).equals("[[")) {
+            s = s.substring(2, s.length() - 1);
+            String res = "";
+
+            int pos = s.indexOf("]"), cur = 0;
+
+            while (pos != -1) {
+                res = res.concat(s.substring(cur, pos).replaceAll("[, ]+", " ").concat("\n"));
+                cur = s.indexOf("[", pos) + 1;
+                if (cur == 0)
+                    break;
+                pos = s.indexOf("]", cur);
+                cnt++;
             }
-        } else {
-            if (selectedValue[1] == 0) {
-                return s.substring(1, s.length() - 1);
-            } else if (selectedValue[1] == 1) {
-                JOptionPane.showMessageDialog(null, "What the heck is 2D string?!", "Title", JOptionPane.QUESTION_MESSAGE);
-            } else {
-                JOptionPane.showMessageDialog(null, "What the heck is 3D string?!", "Title", JOptionPane.QUESTION_MESSAGE);
-            }
+            vecSize.add(cnt);
+            return res;
         }
+
+        else if (s.substring(0, 1).equals("[")) {
+            String tmp = s.substring(1, s.length() - 1).replaceAll("[, ]+", " ");
+
+            for (int i = 0; i < tmp.length(); i++)
+                if (tmp.charAt(i) == ' ')
+                    cnt++;
+
+            vecSize.add(cnt);
+            return tmp;
+        }
+
+        System.out.print("OTHER!");
         return "";
-    }
 
-    class LRadioButton1 implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == array)
-                selectedValue[0] = 0;
-            else
-                selectedValue[0] = 1;
-        }
-    }
-
-    class LRadioButton2 implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == oneDimension)
-                selectedValue[1] = 0;
-            else if (e.getSource() == twoDimension)
-                selectedValue[1] = 1;
-            else
-                selectedValue[1] = 2;
-        }
     }
 
     class LRunButton implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            textArea2.setText(getFromatedText(textArea1.getText()));
+            String[] testcases = textArea1.getText().split("\n\n");
+
+            String res = "";
+            for (String testcase : testcases) {
+                String[] lines = testcase.split("\n");
+                String tmp = "";
+                for (String line : lines)
+                    tmp += getFromatedText(line) + "\n";
+                res += addSizes() + "\n" + tmp + "\n";
+            }
+            textArea2.setText(res);
             textArea2.requestFocus();
             textArea2.select(0, textArea2.getText().length());
+        }
+
+        public String addSizes() {
+            HashSet<Integer> set = new HashSet<>();
+            String res = "";
+
+            for (Integer x : vecSize) {
+                if (!set.contains(x)) {
+                    res += Integer.toString(x) + " ";
+                    set.add(x);
+                }
+            }
+
+            vecSize.clear();
+            return res;
         }
     }
 
@@ -190,11 +171,46 @@ class MainFrame extends JFrame {
         }
     }
 
+    class LConvertStringButton implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String[] text = textArea2.getText().split("\n\n");
+
+            String res = "";
+            for (String tmp : text) {
+                res += tmp.substring(0, tmp.indexOf('\n')) + "\n";
+                tmp = tmp.substring(tmp.indexOf('\n') + 1, tmp.length());
+                res += convertString(tmp) + "\n";
+            }
+            textArea2.setText(res.strip());
+        }
+
+        String convertString(String str) {
+            String res = "";
+            String[] lines = str.split("\n");
+
+            for (String line : lines) {
+                if (line.length() < 3)
+                    continue;
+
+                line = line.substring(1, line.length() - 1);
+                for (int i = 0; i < line.length(); i++)
+                    if (line.charAt(i) == '"') {
+                        res += ' ';
+                        i += 2;
+                    } else {
+                        res += line.charAt(i);
+                    }
+                res += "\n";
+            }
+            return res;
+        }
+    }
+
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
-        mainFrame.setVisible(true);
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setBounds(200, 150, 600, 550);
         mainFrame.setTitle("Leetcode Helper");
-        mainFrame.setBounds(200, 150, 600, 700);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
     }
 }
